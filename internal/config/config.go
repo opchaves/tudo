c467aa2699a7e540f0f74b7a43651a6783c1c314
Paulo Chaves <opaulochaves@gmail.com>
2025-01-06 17:55:05 -0300

    add config package

---
+++ internal/config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+var doOnce sync.Once
+
+type ctxKey int
+
+const (
+	CtxClaims ctxKey = iota
+	CtxRefreshToken
+	CtxVersion
+)
+
+var (
+	Name    = getEnv("TUDO_NAME", "tudo")
+	Env     = getEnv("TUDO_ENV", "development")
+	Host    = getEnv("HOST", "0.0.0.0")
+	Port    = getEnv("PORT", "8080")
+	Origins = getEnv("ORIGINS", "")
+
+	IsProduction = Env == "production"
+	IsLocal      = Env == "development" || Env == "test"
+	DatabaseURL  = getEnv("DATABASE_URL", "postgres://dev:password@localhost:5432/devdb?sslmode=disable")
+
+	JwtSecret        = getEnv("JWT_SECRET", "superSecret")
+	JwtExpiry        = toDuration("JWT_EXPIRY", "1h")
+	JwtRefreshExpiry = toDuration("JWT_REFRESH_EXPIRY", "72h")
+)
+
+func toDuration(envVar string, defaultVal string) time.Duration {
+	val, err := time.ParseDuration(getEnv(envVar, defaultVal))
+	if err != nil {
+		log.Fatalf("Invalid value for %s: %s", envVar, err)
+	}
+	return val
+}
+
+func toInt(envVar string, defaultVal string) int {
+	val, err := strconv.Atoi(getEnv(envVar, defaultVal))
+	if err != nil {
+		log.Fatalf("Invalid value for %s: %s", envVar, err)
+	}
+	return val
+}
+
+func getEnv(name, defaultValue string) string {
+	doOnce.Do(func() {
+		readEnvFile(".env")
+	})
+
+	if value := os.Getenv(name); value != "" {
+		return value
+	}
+
+	return defaultValue
+}
+
+func readEnvFile(filename string) {
+	env := os.Getenv("TUDO_ENV")
+	if env != "production" {
+		err := godotenv.Load(filename)
+		if err != nil {
+			log.Printf("No %s file found. Using default values.\n", filename)
+		}
+	}
+}
