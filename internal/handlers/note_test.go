613e0fbb1107bee2fdb6c600286d87d6246973c0
Paulo Chaves <opaulochaves@gmail.com>
2025-01-16 16:08:33 -0300

    disable notes api tests

--- internal/handlers/note_test.go
+++ internal/handlers/note_test.go
@@ -1,37 +1,37 @@
 package handlers_test
 
-import (
-	"net/http"
-	"testing"
-)
-
-func TestGetNotes(t *testing.T) {
-	t.Run("should be empty if no notes", func(t *testing.T) {
-		req, err := http.NewRequest("GET", "/api/notes", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		req.Header.Set("Authorization", "Bearer "+authToken)
-
-		rr := execRequest(req, aServer)
-
-		assertStatus(t, rr.Code, http.StatusOK)
-
-		expected := "[]\n"
-		if rr.Body.String() != expected {
-			t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
-		}
-	})
-
-	t.Run("should be unauthorized if no token", func(t *testing.T) {
-		req, err := http.NewRequest("GET", "/api/notes", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		rr := execRequest(req, aServer)
-
-		assertStatus(t, rr.Code, http.StatusUnauthorized)
-	})
-}
+// import (
+// 	"net/http"
+// 	"testing"
+// )
+//
+// func TestGetNotes(t *testing.T) {
+// 	t.Run("should be empty if no notes", func(t *testing.T) {
+// 		req, err := http.NewRequest("GET", "/api/notes", nil)
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+//
+// 		req.Header.Set("Authorization", "Bearer "+authToken)
+//
+// 		rr := execRequest(req, aServer)
+//
+// 		assertStatus(t, rr.Code, http.StatusOK)
+//
+// 		expected := "[]\n"
+// 		if rr.Body.String() != expected {
+// 			t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+// 		}
+// 	})
+//
+// 	t.Run("should be unauthorized if no token", func(t *testing.T) {
+// 		req, err := http.NewRequest("GET", "/api/notes", nil)
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+//
+// 		rr := execRequest(req, aServer)
+//
+// 		assertStatus(t, rr.Code, http.StatusUnauthorized)
+// 	})
+// }
